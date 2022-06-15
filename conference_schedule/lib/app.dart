600529a218ff8a7fa0bf0a97b1aab72bcf716f12
Lazar-Toshkin <lazar.toshkin@tide.co>
2022-06-15 13:23:24 +0300

    initial phrase config

--- conference_schedule/lib/app.dart
+++ conference_schedule/lib/app.dart
@@ -1,9 +1,8 @@
 import 'package:conference_schedule/di/di_container.dart';
-import 'package:conference_schedule/localization/app_localizations.dart';
 import 'package:conference_schedule/navigation/router.dart';
 import 'package:conference_schedule/theme/dark_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/phrase_localizations.dart';
 
 class ConferenceScheduleApp extends StatelessWidget {
   const ConferenceScheduleApp({Key? key}) : super(key: key);
@@ -17,13 +16,8 @@ class ConferenceScheduleApp extends StatelessWidget {
       routerDelegate: diContainer<ConferenceScheduleAppRouter>().delegate(),
       routeInformationParser:
           diContainer<ConferenceScheduleAppRouter>().defaultRouteParser(),
-      supportedLocales: AppLocalizations.supportedLocales,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        AppLocalizations.delegate,
-      ],
+      supportedLocales: PhraseLocalizations.supportedLocales,
+      localizationsDelegates: PhraseLocalizations.localizationsDelegates,
     );
   }
 }
--- conference_schedule/lib/main.dart
+++ conference_schedule/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:conference_schedule/app.dart';
 import 'package:conference_schedule/di/di_container.dart';
 import 'package:flutter/material.dart';
+import 'package:phrase/phrase.dart';
 
 void main() async {
+  Phrase.setup("[DISTRIBUTION_ID]", "[ENVIRONMENT_ID]");
   initDI();
   runApp(const ConferenceScheduleApp());
 }
